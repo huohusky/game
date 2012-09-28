05e18d5ac77308ba92679d59d9f9ca54b1ca848a
sky <sky@sky.(none)>
2012-09-28 14:30:38 +0800

    first

---
+++ sort/sort.cpp
@@ -0,0 +1,134 @@
+
+#include <stdio.h>
+
+int swap(int &a, int &b)
+{
+    int c = a;
+    a = b;
+    b = c;
+
+    return 0;
+
+}
+
+int selectSort(int *a, int n)
+{
+    int min;
+    for(int i = 0; i < n; ++i)
+    {
+		min = i;
+		for(int j = i+1; j < n; ++j)
+		{
+		    if(a[j] < a[min])
+		    {
+			min = j;
+		    }
+		}
+		if(i != min)
+		{
+		    swap(a[i], a[min]);
+		}
+	}
+    return 0;
+}
+
+int insertSort(int *a, int n)
+{
+	for(int i = 1; i < n; ++i)
+	{
+		int tmp = a[i];
+		int j;
+		for(j = i; j > 0 && tmp < a[j-1]; --j)
+		{
+			a[j] = a[j-1];
+		}
+		a[j] = tmp;
+	}
+    return 0;
+}
+
+int shellSort(int *a, int n)
+{
+	for(int gap = n/2; gap > 0; gap /= 2)
+	{
+		for(int i = gap; i < n; ++i)
+		{
+			for(int j = i-gap; j > 0 && a[j] > a[j+gap]; j -= gap)
+			{
+				swap(a[j], a[j+gap]);
+			}
+		}
+	}
+    return 0;
+}
+
+int bubbleSort(int *a, int n)
+{
+	for(int i = 0; i < n; ++i)
+	{
+		bool isSwap = false;
+		for(int j = 1; j < n-i; ++j)
+		{
+			if(a[j] < a[j-1])
+			{
+				swap(a[j], a[j-1]);
+				isSwap = true;
+			}
+		}
+		if(!isSwap)
+			break;
+	}
+    return 0;
+}
+
+int quickSort(int *a, int l, int r)
+{
+	if(l < r)
+	{
+		int i = l;
+		int j = r;
+		int x = a[l];
+
+		while(i < j)
+		{
+			while(i < j && a[j] > x)
+				--j;
+
+			if(i < j)
+				a[i++] = a[j];
+
+			while(i < j && a[i] < x)
+				++i;
+
+			if(i < j)
+				a[j--] = a[i];
+		}
+		a[i] = x;
+		quickSort(a, l, i-1);
+		quickSort(a, i+1, r);
+	}
+
+    return 0;
+}
+
+
+int main()
+{
+    int a[] = {1, 99, 22, 34, 65, 3, 87, 1, 23, 21};
+    for(int i = 0; i < 10; ++i)
+    {
+		printf("%d-",a[i]);
+    }
+    printf("\n");
+    //selectSort(a, 10);
+	//insertSort(a, 10);
+	//quickSort(a, 0, 9);
+	//bubbleSort(a, 10);
+	shellSort(a, 10);
+    for(int i = 0; i < 10; ++i)
+    {
+		printf("%d-",a[i]);
+    }
+    printf("\n");
+    return 0;
+}
